b1f82680b9c00b1eadf85a7de7e7bf419ead8391
pranavsinghal2112 <pranavsinghal2112@gmail.com>
2024-09-10 21:58:57 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily: {
+      sans: ['DM Sans', 'sans-serif'],
+    },},
+  },
+  plugins: [],
+}
